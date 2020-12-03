f857402f1b2fe3e0e035bea1c58588e523516306
Will Metcher <will.metcher@gmail.com>
2020-12-03 19:26:42 +1000

    Day 3

---
+++ Day03.py
@@ -0,0 +1,17 @@
+f = open("input.txt","r")
+lines = f.readlines()
+f.close()
+
+
+def slopes(dx, dy):
+    x = 0
+    trees = 0
+    for i in range(0,len(lines),dy):
+        if lines[i][x%31] == "#":
+            trees += 1
+        x += dx
+    return trees
+
+print(slopes(1,1) * slopes(3,1) * slopes(5,1) * slopes(7,1) * slopes(1,2))
+        
+    
